ca356565de973ad1ad4845ccd7a603719e2c15ab
Rohit Movva <tihormovva@gmail.com>
2023-09-17 14:24:48 -0600

    trying some stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,53 +1,70 @@
-#!/bin/bash
+# compile_lamscript.sh
+#
+# This script is used to configure and compile lamscript.
 
-DOCFILES="AUTHORS CONTRIBUTORS COPYING README.md CHANGELOG.md"
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+pushd "$ROOT_DIR" > /dev/null
 
-VERSION=$(sed -n "s/^  version : '\(.*\)',/\1/p" meson.build)
+# ----------------------------- LAMBDA-SH & ARGS ------------------------------
 
-USAGE="Usage: $0 <linux|windows|macos|flatpak|snap>"
+# source lambda-sh/lambda.sh
 
-if [ $# -eq 0 ]; then
-    echo "$USAGE"
-    exit 1
+LAMBDA_PARSE_ARG build Release "The type of build to produce."
+LAMBDA_PARSE_ARG cpp_compiler g++ "The compiler to use for C++ code."
+LAMBDA_PARSE_ARG cores 6 "The number of cores used for compilation."
+LAMBDA_PARSE_ARG os Linux "The operating system being built on."
+
+LAMBDA_COMPILE_ARGS $@
+
+export CXX="$LAMBDA_cpp_compiler"
+
+BUILD_GENERATOR="Unix Makefiles"
+BUILD_COMMAND="make -j $LAMBDA_cores"
+
+if [ "$LAMBDA_os" = "Windows" ]; then
+    BUILD_GENERATOR="Visual Studio 16 2019"
+    BUILD_COMMAND="MSBuild.exe lamscript.sln //t:Rebuild //p:Configuration=$LAMBDA_build"
 fi
 
-set -e -x
+LAMBDA_INFO "Attempting to Compile a $LAMBDA_build for lamscript."
 
+# ----------------------------------- CMAKE ------------------------------------
 case "$1" in
     linux)
-        meson linux_build
-        DESTDIR="$(pwd)/wavbreaker-$VERSION-linux/" ninja -C linux_build install -v
-        mkdir -p dist
-        zip -r "dist/wavbreaker-$VERSION-linux.zip" "wavbreaker-$VERSION-linux" $DOCFILES
-        ;;
-    windows)
-        docker build -t wavbreaker-win32-build scripts/win32
-        sh scripts/win32/run-build.sh
-        rm -rf "wavbreaker-$VERSION-win32"
-        mv wavbreaker-win32 "wavbreaker-$VERSION-win32"
-        mkdir -p dist
-        zip -r "dist/wavbreaker-$VERSION-win32.zip" "wavbreaker-$VERSION-win32" $DOCFILES
-        ;;
-    flatpak)
-        docker build -t wavbreaker-flatpak-build scripts/flatpak
-        sh scripts/flatpak/package.sh
-        ;;
-    snap)
-        snapcraft
-        mkdir -p dist
-        mv wavbreaker_*.snap dist/
+        g++ `pkg-config --cflags gtk+-3.0` -o example-0 SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
         ;;
     macos)
-        meson --prefix=/Applications/wavbreaker.app \
-              --bindir=Contents/MacOS \
-              -Dmacos_app=true \
-              macos_build
-        DESTDIR=$(pwd)/ ninja -C macos_build install -v
-        mkdir -p dist
-        zip -r "dist/wavbreaker-$VERSION-macos.zip" Applications $DOCFILES
+        g++ `pkg-config --cflags gtk+-3.0` -o example-0 SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
+    ;;
+    windows)
+        $cpp_compiler `pkg-config --cflags gtk+-3.0` -o example-0 SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
         ;;
-    *)
-        echo "$USAGE"
-        exit 1
-        ;;
-esac
+
+# mkdir -p build
+# pushd build > /dev/null
+
+# if [ "$LAMBDA_build" = "Release" ] || [ "$LAMBDA_build" = "Debug" ]; then
+#     cmake .. \
+#         -G "$BUILD_GENERATOR" \
+#         -DCMAKE_BUILD_TYPE="$LAMBDA_build"
+# elif [ "$LAMBDA_build" = "Dist" ]; then
+#     cmake .. \
+#         -G "$BUILD_GENERATOR" \
+#         -DCMAKE_BUILD_TYPE=Release
+# else
+#     LAMBDA_FATAL \
+#         "You need to pass a valid build type in order to compile lamscript."
+# fi
+
+# LAMBDA_ASSERT_LAST_COMMAND_OK \
+#     "Couldn't generate the cmake files necessary for compiling lamscript."
+
+# ----------------------------------- BUILD ------------------------------------
+
+$BUILD_COMMAND
+
+LAMBDA_ASSERT_LAST_COMMAND_OK "Couldn't successfully compile lamscript."
+LAMBDA_INFO "Successfully compiled lamscript."
+
+popd > /dev/null  # build
+popd > /dev/null  # ROOT_DIR
