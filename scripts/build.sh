fd70d754cae9af680109075408a2e8bdfb3bcafe
Rohit Movva <tihormovva@gmail.com>
2023-09-19 19:47:56 -0600

    added debug for windows build

--- scripts/build.sh
+++ scripts/build.sh
@@ -40,6 +40,8 @@ case "$LAMBDA_os" in
         ;;
     windows)
         LAMBDA_INFO "Attempting to Compile a $LAMBDA_build for Windows -."
+        pkg-config --cflags gtk+-3.0
+        pkg-config --libs gtk+-3.0
         $LAMBDA_cpp_compiler `pkg-config --cflags gtk+-3.0` -o build SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
         ;;
     *)
