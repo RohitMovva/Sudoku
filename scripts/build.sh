3007a8b8f8a73f060d3b2fd1cb95a430bd28db43
rmovva01 <rmovva01@bvsd.org>
2023-09-21 09:49:04 -0600

    added debug logs for required files

--- scripts/build.sh
+++ scripts/build.sh
@@ -44,8 +44,10 @@ case "$LAMBDA_os" in
         # LAMBDA_INFO $var
         # bash -c "pkg-config --libs gtk+-3.0"
         # bash -c "$LAMBDA_cpp_compiler `pkg-config --cflags gtk+-3.0` -o build SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`"
-        
-        $LAMBDA_cpp_compiler CPPFLAGS=-I/mingw64/include LDFLAGS=-L/mingw64/lib `pkg-config --cflags gtk+-3.0` -o build SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
+        echo `pkg-config --libs gtk+-3.0`
+        echo `pkg-config --cflags gtk+-3.0`
+        # CPPFLAGS=-I/mingw64/include LDFLAGS=-L/mingw64/lib
+        $LAMBDA_cpp_compiler `pkg-config --cflags gtk+-3.0` -o build SudokuSquare.cpp createSudokuPuzzle.cpp test.cpp `pkg-config --libs gtk+-3.0`
         ;;
     *)
         echo "$USAGE"
